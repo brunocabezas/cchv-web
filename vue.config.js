c20c57158c5fa6df1735a774858b850ad4397f49
Bruno Cabezas <bruno.cabezas@gmail.com>
2020-06-01 01:17:06 -0400

    change public path on production

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   lintOnSave: false,
-  transpileDependencies: [
-    /\bvue-awesome\b/
-  ]
-};
+  transpileDependencies: [/\bvue-awesome\b/],
+  publicPath:
+    process.env.NODE_ENV === "production"
+      ? process.env.VUE_APP_ASSETS_URL
+      : "/",
+}
