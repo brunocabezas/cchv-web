b48e0790675f53c300c7f0e26f74b0561c078795
Bruno Cabezas <bruno.cabezas@gmail.com>
2020-08-08 20:38:49 -0400

    style mobile nav menu; add separate style files for header and headerNav

--- src/components/layout/Header/Header.vue
+++ src/components/layout/Header/Header.vue
@@ -26,14 +26,14 @@
       :crossIcon="false"
     >
       <div class="headerRightSection">
-        <div v-if="!onBigScreen" class="socialNetworkContainer">
+        <div v-if="onBigScreen" class="socialNetworkContainer">
           <SocialNetworks small />
         </div>
         <div class="headerNavContainer">
           <HeaderNav />
           <div class="customHeaderNavItem">
             <a
-              class="youtubeChannelLink"
+              class="headerNavItemLink youtubeChannelLink"
               title="Canal de YouTube"
               :href="YOUTUBE_CHANNEL_URL"
               target="_blank"
@@ -42,14 +42,14 @@
               {{ YOUTUBE_CHANNEL_LABEL }}
             </a>
           </div>
-          <div v-if="onBigScreen" class="socialNetworkContainer">
+          <div v-if="!onBigScreen" class="socialNetworkContainer">
             <div class="socialNetworkContainerTitle headerNavItem">
               Redes Sociales
             </div>
             <SocialNetworks color="white" />
           </div>
-          <div class="socialNetworkContainer">
-            <div class="socialNetworkContainerTitle headerNavItem">
+          <div class="contactContainer">
+            <div class="contactContainerTitle headerNavItem">
               Contacto
             </div>
             <a class="headerNavItemLink" href="mailto:info@cchv.cl"
@@ -67,7 +67,7 @@
         <HeaderNav />
         <div class="customHeaderNavItem">
           <a
-            class="youtubeChannelLink"
+            class="headerNavItemLink youtubeChannelLink"
             title="Canal de YouTube"
             :href="YOUTUBE_CHANNEL_URL"
             target="_blank"
@@ -81,116 +81,7 @@
   </div>
 </template>
 <script lang="ts" src="./header.ts"></script>
-<style lang="stylus">
-@import '../../../styles/variables.styl';
-
-.bm-menu
-  background-color: $dark-blue;
-  height: 500px;
-  padding-top: 0;
-  margin-top: $header_mobile_height;
-
-  .bm-item-list
-    padding-left: 0;
-    margin-left: 0;
-
-.header
-  background-color: $bg_color;
-  z-index: 10;
-  display: flex;
-  position: fixed;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: $header_height;
-  align-items: center;
-  padding: 0 10px;
-  border-bottom: 2px solid $grey;
-  transition: 0.2s;
-
-  &.header--small
-    background-color: $blue;
-    height: $header_mobile_height;
-    position: relative;
-
-    .headerLeftSection, .headerRightSection
-      height: $header_mobile_height;
-
-  .headerLeftSection
-    display: flex;
-    height: $header_height;
-    align-items: center;
-    width: 300px;
-
-  .headerRightSection
-    display: flex;
-    flex: 1;
-    height: $header_height;
-    width: 70%;
-    flex-direction: column;
-
-    @media (max-width: $md)
-      width: 100%;
-
-    &--with-button
-      @media (max-width: $md)
-        position: initial;
-
-      padding-right: 10px;
-      justify-content: center;
-      align-items: flex-end;
-
-    .headerNavContainer
-      display: flex;
-      flex: 1;
-
-      @media (max-width: $md)
-        flex-direction: column;
-
-      .youtubeChannelLink
-        font-size: 18px;
-
-.customHeaderNavItem
-  // as headerNavItemLink
-  a
-    display: flex;
-    align-items: center;
-    height: 100%;
-    font-family: Montserrat;
-    width: 100%;
-    padding: 0 25px;
-
-    &:hover
-      background-color: $grey;
-
-    @media (max-width: $md)
-      font-size: 18px;
-      padding-bottom: 5px;
-      margin-bottom: 15px;
-      border-bottom: 1px solid white;
-      color: white;
-
-// Only on @media (max-width: $md)
-.socialNetworkContainer
-  padding: 0 0 0 25px;
-  margin-top: 10px;
-  text-align: right;
-
-  .headerNavItemLink
-    font-style: 16px;
-    color: white;
-
-  .socialNetworkContainerTitle
-    width: 100%;
-    padding-bottom: 5px;
-    margin-bottom: 15px;
-    border-bottom: 1px solid #fff;
-    color: #fff;
-    text-transform: uppercase;
-    font-family: Montserrat;
-    font-size: 18px;
-</style>
-
+<style lang="stylus" src="./header.styl"></style>
 <style lang="stylus">
 @import '../../../styles/variables.styl';
 
--- src/components/layout/Header/HeaderNav.vue
+++ src/components/layout/Header/HeaderNav.vue
@@ -8,7 +8,12 @@
         'headerNavItem--nested': menu.pages && menu.pages.length
       }"
     >
-      <router-link v-if="menu.url" :title="menu.label" :to="menu.url">
+      <router-link
+        class="headerNavItemLink"
+        v-if="menu.url"
+        :title="menu.label"
+        :to="menu.url"
+      >
         {{ menu.label }}
       </router-link>
 
@@ -25,13 +30,14 @@
             class="headerNavItemSubMenu"
           >
             <a
+              class="headerNavItemLink"
               v-if="submenu.is_external"
               target="_blank"
               :title="submenu.label"
               :href="submenu.url"
               >{{ submenu.label }}</a
             >
-            <router-link :to="submenu.url" v-else>{{
+            <router-link class="headerNavItemLink" :to="submenu.url" v-else>{{
               submenu.label
             }}</router-link>
           </div>
@@ -40,101 +46,5 @@
     </div>
   </div>
 </template>
-
 <script lang="ts" src="./headerNav.ts"></script>
-
-<style scoped lang="stylus">
-@import '../../../styles/variables.styl';
-
-.headerNavItem
-  display: flex;
-  align-items: center;
-  padding: 0;
-  position: relative;
-
-  @media (max-width: $md)
-    flex-direction: column;
-    width: 100%;
-    // Display a line on the bottom of each menu
-    border-bottom: 1px solid white;
-    margin-bottom: 10px;
-    padding-bottom: 10px;
-    align-items: flex-start;
-
-  &:hover
-    .headerNavItemSubMenuContainer
-      display: block;
-
-  .headerNavItemSubMenuContainer
-    display: none;
-
-    @media (max-width: $md)
-      display: block;
-      width: 100%;
-
-  > div
-    height: 100%;
-
-  a, .headerNavItemLink
-    color: $blue;
-    display: flex;
-    height: 100%;
-    align-items: center;
-    font-family: Montserrat;
-    text-transform: uppercase;
-    padding: 1em 1em;
-    transition: all 0.2s;
-
-    @media (max-width: $md)
-      color: white;
-      width: 100%;
-      padding: 5px 0;
-
-    &:hover
-      background-color: $grey;
-
-  a:hover
-    @media (max-width: $md)
-      text-decoration: underline;
-
-  .headerNavItemLink
-    @media (max-width: $md)
-      underline();
-
-.headerNav
-  display: flex;
-  justify-content: flex-end;
-  flex: 1;
-  font-size: 18px;
-
-  @media (max-width: $md)
-    flex-direction: column;
-    align-items: flex-start;
-    padding: 0;
-
-  .headerNavItemSubLevel
-    position: absolute;
-    background-color: white;
-    top: 64px;
-    left: 0;
-    z-index: 2;
-    white-space: nowrap;
-    box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
-
-    @media (max-width: $md)
-      position: initial;
-      background-color: $dark-blue;
-      text-align: right;
-      box-shadow: none;
-
-    .headerNavItemSubMenu
-      border-top: 1px solid $blue;
-      text-transform: uppercase;
-      font-size: 14px;
-
-      &:hover
-        background-color: darken($grey, 3);
-
-        a
-          height: auto;
-</style>
+<style scoped lang="stylus" src="./headerNav.styl"></style>
