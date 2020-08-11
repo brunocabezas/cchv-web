36edff254b89adeccdd8055aa42ce4e341803555
Bruno Cabezas <bruno.cabezas@gmail.com>
2020-08-10 20:47:22 -0400

    display white logo

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,28 +1,44 @@
 <template>
   <router-link class="logo" :to="homeUrl" title="Inicio">
-    <img alt="Vue logo" src="@/assets/logo.png" />
+    <img v-if="onBigScreen" alt="Vue logo" src="@/assets/logo.png" />
+    <img v-else alt="Vue logo" src="@/assets/white-logo.png" />
   </router-link>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "@vue/composition-api";
+import { defineComponent, computed } from "@vue/composition-api";
 import Urls from "@/utils/urls";
+import useMediaQueries from "@/hooks/useMediaQueries";
 
 const Logo = defineComponent({
   name: "Logo",
   setup() {
-    return { homeUrl: Urls.Home };
+    const { onBigScreen } = useMediaQueries();
+
+    const logoUrl = computed<string>(() =>
+      onBigScreen.value ? "@/assets/logo.png" : "@/assets/white-logo.png"
+    );
+
+    return { onBigScreen, homeUrl: Urls.Home, logoUrl };
   }
 });
 
 export default Logo;
 </script>
 <style lang="stylus">
+@import '../styles/variables.styl';
+
 .logo
   display: flex;
   width: 300px;
-  padding: 0 30px;
+  align-items: center;
 
   img
     width: 100%;
+    padding: 0 30px;
+
+    @media (max-width: $md)
+      width: auto;
+      height: 50px;
+      padding: 0;
 </style>
