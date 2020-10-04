df015f6010cb5e99c37d054e5602112a11575d6a
Bruno Cabezas <bruno.cabezas@gmail.com>
2020-10-04 19:47:02 -0400

    add totalPages ref on newsPage

--- src/components/News/NewsPage/newsPage.ts
+++ src/components/News/NewsPage/newsPage.ts
@@ -1,18 +1,29 @@
-import { computed, defineComponent, ref } from "@vue/composition-api"
+import { computed, defineComponent, onMounted, ref } from "@vue/composition-api"
 import NewsPostThumbnail from "@/components/News/NewsPostThumbnail.vue"
 import Loader from "@/components/Loader.vue"
 import useNews from "@/models/useNews"
 import useMediaQueries from "@/hooks/useMediaQueries"
 
-// Used with infinite loading
+// News are loaded when the user scrolls down
+// An infinite loader is displayed
+// Wordpress pagination is utilized
+
+// Number of news to fetch when the user scrolls down
 const NEWS_PER_PAGE = 6
-// Initial news per page used to fetch first five news
-export const INITIAL_NEWS_BY_PAGE = 5
+
+// Used on the first network request, to initialize state with N news
+export const INITIAL_NEWS = 6
+
+const NO_PAGES_INDICATOR = -9999
+
+// Wordpress header containing the total amount of items to be fetched
+// https://developer.wordpress.org/rest-api/using-the-rest-api/pagination/
 const TOTAL_PAGES_HEADER = "x-wp-totalpages"
 
 interface infiniteHandlerState {
   loaded: () => void
   // Stops fetching
+  reset: () => void
   complete: () => void
 }
 
@@ -29,40 +40,53 @@ export default defineComponent({
       setNewsPage,
     } = useNews()
     const { onBigScreen } = useMediaQueries()
+    const totalPages = ref(-1)
     const initialDataLoading = ref(false)
 
-    if (news.value.length === 0) {
-      initialDataLoading.value = true
-
-      // Fetch
-      fetchNews(
-        { per_page: INITIAL_NEWS_BY_PAGE, page: page.value },
-        true
-      ).finally(() => {
-        initialDataLoading.value = false
-      })
-      setNewsPage(page.value + 1)
-    } else if (page.value === 1) {
-      // If there are news defined and the page is equal to one, set it to 2
-      setNewsPage(2)
-    }
+    onMounted(() => {
+      // Fetching data initially
+      if (news.value.length === 0) {
+        initialDataLoading.value = true
+        const options = { per_page: 6, page: page.value }
+        fetchNews(options, true)
+          .then((response) => {
+            const totalPagesFromHeader =
+              (response &&
+                response.headers &&
+                response.headers[TOTAL_PAGES_HEADER]) ||
+              NO_PAGES_INDICATOR
+            // Set total pages from response header
+            totalPages.value = parseInt(totalPagesFromHeader, 10)
+            setNewsPage(page.value + 1)
+          })
+          .catch(() => {
+            totalPages.value = NO_PAGES_INDICATOR
+          })
+          .finally(() => {
+            initialDataLoading.value = false
+          })
+      }
+    })
 
     function infiniteHandler($state: infiniteHandlerState) {
       const params = { per_page: NEWS_PER_PAGE, page: page.value }
-      fetchNews(params, true).then((response) => {
-        // Get total pages from headers
-        const totalPages =
-          (response &&
-            response.headers &&
-            response.headers[TOTAL_PAGES_HEADER]) ||
-          0
-
-        // Stop fetch checking at totalPages
-        if (page.value < totalPages) {
-          setNewsPage(page.value + 1)
-          $state.loaded()
+      fetchNews(params, true).then(() => {
+        if (totalPages.value >= 0) {
+          // Stop fetch checking at totalPages
+          if (page.value < totalPages.value) {
+            setNewsPage(page.value + 1)
+            $state.loaded()
+          } else {
+            $state.complete()
+          }
         } else {
-          $state.complete()
+          // If NO_PAGES_INDICATOR is used, there are no news available
+          if (totalPages.value === NO_PAGES_INDICATOR) {
+            $state.complete()
+          } else {
+            // handler state will be reseted unit onMounted
+            $state.reset()
+          }
         }
       })
     }
@@ -74,6 +98,7 @@ export default defineComponent({
       initialDataLoading,
       isLoading: computed(() => isLoading.value),
       onBigScreen,
+      totalPages,
     }
   },
 })
