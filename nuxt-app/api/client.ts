31a5b82784a8bbfb26f1a41dff385e69969f4cd5
Bruno Cabezas <bruno.cabezas@gmail.com>
2020-10-24 11:38:47 -0300

    add api folder

---
+++ nuxt-app/api/client.ts
@@ -0,0 +1,19 @@
+import axios from "axios"
+// import setupMocks from './mocks';
+
+const BASE_URL: string = process.env.VUE_APP_BASE_URL || "http://localhost"
+
+if (!process.env.VUE_APP_BASE_URL) {
+  console.warn(`API base url is not defined, it will default to ${BASE_URL}`)
+}
+
+const client = axios.create({
+  baseURL: BASE_URL,
+})
+
+const enableMocks = process.env.VUE_APP_ENABLE_MOCKS;
+if (enableMocks) {
+  // setupMocks(client);  
+}
+
+export default client
